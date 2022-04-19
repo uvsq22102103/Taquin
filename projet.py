b6e6725cc45be9f3351dcf8a839f42370cd2c523
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-04-19 12:41:14 +0200

    update

--- projet.py
+++ projet.py
@@ -1,10 +1,26 @@
 import tkinter as tk
 import random as rd
 
-######################
+########CONSTANTES#########
 WIDTH, HEIGHT = 800, 800
 BRICK = WIDTH/4
-######################
+###########################
+########FONCTIONS##########
+
+
+def clic(event):
+    global grillage
+    x, y = event.x, event.y
+    obj = canvas.find_closest(x, y)[0]
+    if obj % 2:  # Carré cliqué
+        ens = [obj, obj+1]  # Carré + Text ID
+        print(grillage.index())
+    else:  # Text cliqué
+        ens = [obj, obj+1]  # Carré + Text ID
+        print(grillage.index([obj-1, obj]))
+
+
+###########################
 
 root = tk.Tk()
 root.title("Taquin")
@@ -15,7 +31,6 @@ canvas.grid()
 nbr = []
 for i in range(16):
     nbr.append(i+1)
-print(nbr)
 grillage = []
 for x in range(4):
     for y in range(4):
@@ -23,14 +38,19 @@ for x in range(4):
         del nbr[nbr.index(tirage)]
         if tirage != 16:
             grillage.append([canvas.create_rectangle(x*BRICK+5,
-                                                    y*BRICK+5,
-                                                    x*BRICK+BRICK-5,
-                                                    y*BRICK+BRICK-5,
-                                                    fill="gray",
-                                                    tags="brick"),
+                                                     y*BRICK+5,
+                                                     x*BRICK+BRICK-5,
+                                                     y*BRICK+BRICK-5,
+                                                     fill="gray",
+                                                     tags="brick"),
                             canvas.create_text(x*BRICK+BRICK/2,
-                                                y*BRICK+BRICK/2,
-                                                text=tirage,
-                                                font=('Helvetica', '60'))])
+                                               y*BRICK+BRICK/2,
+                                               text=tirage,
+                                               font=('Helvetica', '60'),
+                                               tags="brick")])
+        else:
+            grillage.append([None])
+print(grillage)
+canvas.tag_bind("brick", "<Button-1>", clic)
 
 root.mainloop()
