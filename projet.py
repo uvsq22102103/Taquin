ec241cee4e54de7e7a5e978a977454530188baa9
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-04-19 12:08:17 +0200

    Impoooort

--- projet.py
+++ projet.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random as rd
 
 ######################
 WIDTH, HEIGHT = 800, 800
@@ -7,12 +8,29 @@ BRICK = WIDTH/4
 
 root = tk.Tk()
 root.title("Taquin")
-canvas = tk.Canvas(root,width=WIDTH,height=HEIGHT,bg="brown")
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="brown")
 canvas.grid()
 
+
+nbr = []
+for i in range(16):
+    nbr.append(i+1)
+print(nbr)
 grillage = []
 for x in range(4):
     for y in range(4):
-        canvas.create_rectangle(x*BRICK+5,y*BRICK+5,x*BRICK+BRICK-5,y*BRICK+BRICK-5,fill="gray",tags="brick")
+        tirage = rd.choice(nbr)
+        del nbr[nbr.index(tirage)]
+        if tirage != 16:
+            grillage.append([canvas.create_rectangle(x*BRICK+5,
+                                                    y*BRICK+5,
+                                                    x*BRICK+BRICK-5,
+                                                    y*BRICK+BRICK-5,
+                                                    fill="gray",
+                                                    tags="brick"),
+                            canvas.create_text(x*BRICK+BRICK/2,
+                                                y*BRICK+BRICK/2,
+                                                text=tirage,
+                                                font=('Helvetica', '60'))])
 
 root.mainloop()
