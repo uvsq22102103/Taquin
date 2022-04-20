00cfa043f1e4d8375483680302d2490a4b178825
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-04-20 17:00:07 +0200

    ça marche mais c'est pas très beau

--- projet.py
+++ projet.py
@@ -8,16 +8,42 @@ BRICK = WIDTH/4
 ########FONCTIONS##########
 
 
+def voisinage(index: int):
+    if index in [0, 1, 2, 5, 6, 9, 10, 13, 14, 15]:
+        p_move = [index-4, index+4, index-1, index+1]
+    elif index in [4, 8, 12]:
+        p_move = [index-4, index+4, index+1]
+    elif index in [3, 7, 11]:
+        p_move = [index-4, index+4, index-1]
+    cpt = 0
+    for i in range(len(p_move)):
+        i -= cpt
+        if not 0 <= p_move[i] <= 15:
+            del p_move[i]
+            cpt += 1
+    return(p_move)
+
+
 def clic(event):
     global grillage
     x, y = event.x, event.y
     obj = canvas.find_closest(x, y)[0]
     if obj % 2:  # Carré cliqué
         ens = [obj, obj+1]  # Carré + Text ID
-        print(grillage.index())
+        p_move = voisinage(grillage.index(ens))
     else:  # Text cliqué
-        ens = [obj, obj+1]  # Carré + Text ID
-        print(grillage.index([obj-1, obj]))
+        ens = [obj-1, obj]  # Carré + Text ID
+        p_move = voisinage(grillage.index(ens))
+    for move in p_move:
+        if grillage[move] is None:
+            for i in range(len(grillage)):
+                if grillage[i] == ens:
+                    grillage[i] = None
+                    break
+            grillage[move] = ens
+            
+            canvas.moveto(ens[0], (move % 4)*BRICK+5, (move//4)*BRICK+5)
+            canvas.moveto(ens[1], (move % 4)*BRICK+50, (move//4)*BRICK+50)
 
 
 ###########################
@@ -32,8 +58,8 @@ nbr = []
 for i in range(16):
     nbr.append(i+1)
 grillage = []
-for x in range(4):
-    for y in range(4):
+for y in range(4):
+    for x in range(4):
         tirage = rd.choice(nbr)
         del nbr[nbr.index(tirage)]
         if tirage != 16:
@@ -49,8 +75,9 @@ for x in range(4):
                                                font=('Helvetica', '60'),
                                                tags="brick")])
         else:
-            grillage.append([None])
+            grillage.append(None)
 print(grillage)
+
 canvas.tag_bind("brick", "<Button-1>", clic)
 
 root.mainloop()
--- projet_aymeric.py
+++ projet_aymeric.py
@@ -1,18 +1,83 @@
 import tkinter as tk
+import random as rd
 
-######################
+########CONSTANTES#########
 WIDTH, HEIGHT = 800, 800
 BRICK = WIDTH/4
-######################
+###########################
+########FONCTIONS##########
+
+
+def voisinage(index: int):
+    if index in [0, 1, 2, 5, 6, 9, 10, 13, 14, 15]:
+        p_move = [index-4, index+4, index-1, index+1]
+    elif index in [4, 8, 12]:
+        p_move = [index-4, index+4, index+1]
+    elif index in [3, 7, 11]:
+        p_move = [index-4, index+4, index-1]
+    cpt = 0
+    for i in range(len(p_move)):
+        i -= cpt
+        if not 0 <= p_move[i] <= 15:
+            del p_move[i]
+            cpt += 1
+    return(p_move)
+
+
+def clic(event):
+    global grillage
+    x, y = event.x, event.y
+    obj = canvas.find_closest(x, y)[0]
+    if obj % 2:  # Carré cliqué
+        ens = [obj, obj+1]  # Carré + Text ID
+        p_move = voisinage(grillage.index(ens))
+    else:  # Text cliqué
+        ens = [obj-1, obj]  # Carré + Text ID
+        p_move = voisinage(grillage.index(ens))
+    for move in p_move:
+        if grillage[move] is None:
+            for i in range(len(grillage)):
+                if grillage[i] == ens:
+                    grillage[i] = None
+                    break
+            grillage[move] = ens
+            
+            canvas.moveto(ens[0], (move % 4)*BRICK+5, (move//4)*BRICK+5)
+            canvas.moveto(ens[1], (move % 4)*BRICK+50, (move//4)*BRICK+50)
+
+
+###########################
 
 root = tk.Tk()
 root.title("Taquin")
-canvas = tk.Canvas(root,width=WIDTH,height=HEIGHT,bg="brown")
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="brown")
 canvas.grid()
 
+
+nbr = []
+for i in range(16):
+    nbr.append(i+1)
 grillage = []
-for x in range(4):
-    for y in range(4):
-        canvas.create_rectangle(x*BRICK+5,y*BRICK+5,x*BRICK+BRICK-5,y*BRICK+BRICK-5,fill="gray",tags="brick")
+for y in range(4):
+    for x in range(4):
+        tirage = rd.choice(nbr)
+        del nbr[nbr.index(tirage)]
+        if tirage != 16:
+            grillage.append([canvas.create_rectangle(x*BRICK+5,
+                                                     y*BRICK+5,
+                                                     x*BRICK+BRICK-5,
+                                                     y*BRICK+BRICK-5,
+                                                     fill="gray",
+                                                     tags="brick"),
+                            canvas.create_text(x*BRICK+BRICK/2,
+                                               y*BRICK+BRICK/2,
+                                               text=tirage,
+                                               font=('Helvetica', '60'),
+                                               tags="brick")])
+        else:
+            grillage.append(None)
+print(grillage)
+
+canvas.tag_bind("brick", "<Button-1>", clic)
 
 root.mainloop()
